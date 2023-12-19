328f5846eee220a6b2261115d6cb6200eb1a17c6
jaleks3 <s24592@pjwstk.edu.pl>
2023-12-19 12:30:46 +0100

    Started working on the board logic

--- Chess/src/Model/Board.java
+++ Chess/src/Model/Board.java
@@ -1,5 +1,13 @@
 package Model;
 
 public class Board {
+    private int[] square = new int[64];
+
+    private final int whiteIndex = 0;
+    private final int blackIndex = 1;
+
+    private boolean whiteToMove;
+
+
 
 }
